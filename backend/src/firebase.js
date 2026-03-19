e01aaf5384afada60a0d07a1af9c377b176c4ee2
jbscika <jbscika@gmail.com>
2026-03-19 15:38:39 -0300

    Update firebase.js

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -1,10 +1,16 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+try {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  console.log('Firebase inicializado com sucesso!');
+} catch (error) {
+  console.error('Erro ao inicializar Firebase:', error.message);
+}
 
 const db = admin.firestore();
 
